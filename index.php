3e1082e8626dfc990ce4ad1fea0b80f14ffac312
lawintun <53369035+lawintun@users.noreply.github.com>
2022-06-17 00:51:49 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo " HELLO "; 
+class currentDt{
+  public $hr;
+  public $yr;
+
+function set_hr($hr){
+  $this->hr = $hr;
+
+ }
+function get_hr(){
+  return $this->hr;
+}
+}
+$current = new currentDT();
+current->set_hr(date("H"));
+echo current->get_hr();
+
+ 
 ?>
