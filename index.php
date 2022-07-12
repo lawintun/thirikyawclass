5cd5fb973c7c7872a704e73a96d532dda393d666
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-12 16:45:38 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <html>
- <head><title></title></head>
+ <head><title></title>
+ <script>
+  function fun() {
+  document.getElementById("sng").play();
+}
+ </script>
+ </head>
    <body>
-      <audio autoplay loop>
+      <audio id="sng" loop>
         <source src="music.mp3">
       </audio>
+   <button onclick="fun()" >PLAY</button>
    </body>
 </html>
