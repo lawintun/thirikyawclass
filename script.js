0492c5fa2c72cbbc326bb1f3647603dae5924008
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-13 00:45:26 +0630

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,24 @@
   function fun() {
   document.getElementById("sng").play();
 }
+const first = document.querySelector(".first");
+const iframe = document.querySelector("iframe");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  var html = first.textContent;
+  iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+});
+
+
+first.addEventListener('keyup',()=>{
+  var html = first.textContent;
+  iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+})
+
+first.addEventListener("paste", function(e) {
+        e.preventDefault();
+        var text = e.clipboardData.getData("text/plain");
+        document.execCommand("insertText", false, text);
+    });
+Css code
