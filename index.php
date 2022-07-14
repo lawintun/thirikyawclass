1918a9d77a3253f65d29d6884bdc2caec054cebe
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-14 18:05:41 +0630

    Update index.php

--- index.php
+++ index.php
@@ -5,21 +5,7 @@
   <link rel="stylesheet" href="style.css">
  </head>
    <body class="bg">
-      <audio id="sng" loop>
-        <source src="music.mp3"> <hr>
-      </audio>
-    
-    <div class="container">
-  <div class="center">
-     <h2>Lawintun's Classwork </h2> <br>
-</div>
-</div>
-<div class="container">
-<div class="center">
-    <button onclick="fun()" >PLAY</button> 
-  </div>
-</div>
-   <hr>
+      
     
     
    </body>
