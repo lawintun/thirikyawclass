e64f2b6cbd46afba63075e5b55ba92ca2b15fb43
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-14 21:21:24 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <html>
  <head><title></title>
   <script type="text/javascript" src="script.js"></script>
- 
+ <script src="three.r119.min.js"></script>
+<script src="vanta.clouds.min.js"></script>
+<script>
+VANTA.CLOUDS({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  skyColor: 0x21bed4,
+  cloudColor: 0xbfbfe1,
+  cloudShadowColor: 0x426b91,
+  sunColor: 0xf8f8f8,
+  sunGlareColor: 0xac03f3,
+  sunlightColor: 0xffb400,
+  speed: 1.50
+})
+</script>
   <link rel="stylesheet" href="style.css">
  </head>
    <body class="bg">
