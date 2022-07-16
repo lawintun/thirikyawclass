5f31e354a1963e22371744e0c19564e2975956f3
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-17 00:10:38 +0630

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,9 @@
   <link rel="stylesheet" href="style.css">
  </head>
    <body>
-
-    <audio id="sng" loop>
-        <source src="music.mp3"> <hr>
-      </audio>
-<audio id="snd" loop>
-        <source src="Khinkhinhtar.mp3"> <hr>
-      </audio>
-<center">
-    <button onclick="fun()" >PLAY</button> 
-</center>
-   <hr>
-<center">
-    <button onclick="func()" >Play</button> 
-</center>
-
+<div class="hero-image">
+  
+</div>
+    
    </body>
 </html>
