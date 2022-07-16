2771e9912fd43b327db7581b18ca7c3de8052151
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-16 18:35:05 +0630

    Update index.php

--- index.php
+++ index.php
@@ -4,25 +4,7 @@
   <link rel="stylesheet" href="style.css">
  </head>
    <body>
-<div class="container">
-<div class="center">
-    <button onclick="fun()" >PLAY</button> 
-  </div>
-</div>
-   <hr>
-    <ul class="slideshow">
-  <li><span>Image 01</span><div><h3>A little something something</h3></div></li>
-  <li><span>Image 02</span></li>
-  <li><span>Image 03</span></li>
-  <li><span>Image 04</span></li>
-  <li><span>Image 05</span></li>
-  <li><span>Image 06</span></li>
-</ul>
-  <div class="container">
-    <header>
-      <h1>CSS3 <span>Fullscreen Slideshow</span></h1>
-  </header>
-</div>
+
     
    </body>
 </html>
