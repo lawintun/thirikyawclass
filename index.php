293920349ee0bf4f7bfec0e6ce63c7d41dea5c4a
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-17 16:54:13 +0630

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
   <link rel="stylesheet" href="style.css">
  </head>
    <body>
-  <div class="vdoview">
+  <center>
   <iframe src="https://drive.google.com/file/d/1PqfKUuSmbvxcZXmVJF1PsdUEHNPtX91-/preview" 
 width="640" height="480" allow="autoplay"></iframe>
-    </div> 
+    </center> 
    </body>
 </html>
