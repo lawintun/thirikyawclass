c54dc12989583ab2e0b70a9373b3608c4b671fd6
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 03:04:41 +0630

    Update script.js

--- script.js
+++ script.js
@@ -9,25 +9,6 @@ function func() {
 }
 
 
-function runMyfunct(document.getElementById('uname').value, document.getElementById('pwd').value){    
-var name = document.getElementById('uname').value;
-var password = document.getElementById('pwd').value;
-if (name==null || name==""){  
-  alert("Name can't be blank !.\n");  
-  return false;  
+function myFunction(){
+  alert("welcome");
 }
-
-else if((name == "DawPyae192168")&&(password == "Mikoyan35")){
-   window.location.replace("home.php");
-   return true;
-
-
-}else if(password.length<9){  
-  alert("Password must be at least 9 characters long.");  
-  return false;  
-  }  
- else if (password == " "){
-  alert ("password field can't be empty !.\n");
- return false;
-}
-}  
