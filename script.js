06b2ca09f8227fab4350f1a9bd648762cf06d2b2
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 03:01:06 +0630

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ function func() {
 }
 
 
-function runMyfunct(){  
-var name=document.myform.uname.value;  
-var password=document.myform.pwd.value;  
-  
+function runMyfunct(document.getElementById('uname').value, document.getElementById('pwd').value){    
+var name = document.getElementById('uname').value;
+var password = document.getElementById('pwd').value;
 if (name==null || name==""){  
   alert("Name can't be blank !.\n");  
   return false;  
