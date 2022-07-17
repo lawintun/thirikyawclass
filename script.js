592c9f995e4fb3679b3aef858dd175a88f5668b0
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 02:28:24 +0630

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ function func() {
 
   document.getElementById("snd").play();
 }
+
+
+function runMyFunct(fld) {
+    var error = "";
+    var illegalChars = /\W/; // allow letters, numbers, and underscores
+ 
+    if (fld.value == "") {
+        fld.style.background = 'Yellow';
+        error = "You didn't enter a username.\n";
+        alert(error);
+        return false;
+ 
+    } else if ((fld.value.length < 5) || (fld.value.length > 15)) {
+        fld.style.background = 'Yellow';
+        error = "The username is the wrong length.\n";
+		alert(error);
+		return false;
+ 
+    } else if (illegalChars.test(fld.value)) {
+        fld.style.background = 'Yellow';
+        error = "The username contains illegal characters.\n";
+		alert(error);
+		return false;
+ 
+    } else {
+        fld.style.background = 'White';
+    }
+    return true;
+}
