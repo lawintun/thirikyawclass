a2be4c2ab9b2c59179a7d4ce3152091f46ed42ce
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 04:07:21 +0630

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,17 @@ function func() {
 
 
 function myfunction(){
-    if(document.getElementById('uname').value == 'DawPyae192168') {
+    if((document.getElementById('uname').value == 'DawPyae192168')&&( document.getElementById('pwd').value == 'Mikoyan35')) {
     window.location.replace("home.php");
-}   
-    
+}   else if (document.getElementById('uname').value !=='DawPyae192168'){
+    alert("Username is invalid ! . \n");
+ }
+    else if (document.getElementById('pwd').value !== 'Mikoyan35') {
+    alert("Password is invalid ! .\n");
+ }
+    else {
+    alert("You need to fill valid username and password ! .\n");
+  }
 }  
 
 
