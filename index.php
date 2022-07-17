182e276f76a2320be2d4e08d151593b30c0ba438
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-17 17:18:49 +0630

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,35 @@
 <?php
     header('X-Frame-Options: GOFORIT'); 
 ?>
+
+<nav class="navbar">
+     <!-- LOGO -->
+     <div class="logo">MUO</div>
+     <!-- NAVIGATION MENU -->
+     <ul class="nav-links">
+       <!-- USING CHECKBOX HACK -->
+       <input type="checkbox" id="checkbox_toggle" />
+       <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+       <!-- NAVIGATION MENUS -->
+       <div class="menu">
+         <li><a href="/">Home</a></li>
+         <li><a href="/">About</a></li>
+         <li class="services">
+           <a href="/">Services</a>
+           <!-- DROPDOWN MENU -->
+           <ul class="dropdown">
+             <li><a href="/">Dropdown 1 </a></li>
+             <li><a href="/">Dropdown 2</a></li>
+             <li><a href="/">Dropdown 2</a></li>
+             <li><a href="/">Dropdown 3</a></li>
+             <li><a href="/">Dropdown 4</a></li>
+           </ul>
+         </li>
+         <li><a href="/">Pricing</a></li>
+         <li><a href="/">Contact</a></li>
+       </div>
+     </ul>
+   </nav>
   <center>
   <iframe src="https://drive.google.com/file/d/1PqfKUuSmbvxcZXmVJF1PsdUEHNPtX91-/preview" 
 width="640" height="480" allow="autoplay"></iframe>
