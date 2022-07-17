01d6c3c073345724f8a50f46f2158a8c32af6101
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 05:22:02 +0630

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ function func() {
 function myfunction(){
 let uname = document.getElementById('uname').value;
 let pwd = document.getElementById('pwd').value;
-    if(uname == 'DawPyae192168' && pwd == 'DawPyae192168'){ 
+    if(uname == 'DawPyae192168'){ 
+  if (pwd == 'DawPyae192168') {
     window.location.replace("home.php");
-   }  
+  }
+}
  
     else {
     alert("You need to fill valid password ! ");
