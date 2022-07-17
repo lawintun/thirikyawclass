cf8248d3c422f725de0b5471bf8a3ddc8d3f9bc7
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-17 22:23:27 +0630

    Update index.php

--- index.php
+++ index.php
@@ -18,9 +18,26 @@
   <iframe class="responsive-iframe" src="https://drive.google.com/file/d/1QR5aE3rO0gmpUVy0J1WQFBRpW9wqGIwZ/preview" allow="autoplay"></iframe>
 </div>
 
-<div class="form-floating">
-  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-  <label for="floatingPassword">Password</label>
+<div class="login-box">
+  <h2>Login</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required="">
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
 </div>
+
    </body>
 </html>
