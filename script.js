7aee92e507358b31f81bf852118d8272067235a7
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 03:20:32 +0630

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function myfunction(){
   if (document.getElementById("uname").value == " "){  
    alert("Username can't be empty ! .\n");   
 }  
-  else if ( document.getElementById("pwd).value == " "){
+  else if ( document.getElementById("pwd").value == " "){
   alert("Password field can't be empty ! .\n");
 }   
 
