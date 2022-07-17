0c8c50f1fde6a97fdd8304196d6e0ab40369ebab
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 02:40:24 +0630

    Update script.js

--- script.js
+++ script.js
@@ -9,30 +9,15 @@ function func() {
 }
 
 
-function runMyfunct(fld) {
-    var error = "";
-    var illegalChars = /\W/; // allow letters, numbers, and underscores
- 
-    if (fld.value == "") {
-        fld.style.background = 'Yellow';
-        error = "You didn't enter a username.\n";
-        alert(error);
-        return false;
- 
-    } else if ((fld.value.length < 5) || (fld.value.length > 15)) {
-        fld.style.background = 'Yellow';
-        error = "The username is the wrong length.\n";
-		alert(error);
-		return false;
- 
-    } else if (illegalChars.test(fld.value)) {
-        fld.style.background = 'Yellow';
-        error = "The username contains illegal characters.\n";
-		alert(error);
-		return false;
- 
-    } else {
-        fld.style.background = 'White';
-    }
-    return true;
-}
+function runMyfunct(){  
+var name=document.myform.name.value;  
+var password=document.myform.password.value;  
+  
+if (name==null || name==""){  
+  alert("Name can't be blank");  
+  return false;  
+}else if(password.length<6){  
+  alert("Password must be at least 6 characters long.");  
+  return false;  
+  }  
+}  
