b976cf6fda83f2a0b7cccc1ee1e7414319dc0987
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 04:30:09 +0630

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,6 @@
   <h2>The Whole Welcome Memo</h2>
   <form method="post" name="myform" id="myform" >
     <div class="user-box">
-      <input type="text" id="uname" name="uname" required>
-      <label>Username</label>
-    </div>
-    <div class="user-box">
       <input type="password" id="pwd" name="pwd" required>
       <label>Password</label>
     </div>
