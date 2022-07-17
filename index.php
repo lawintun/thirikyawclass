d60e8bc642e43cdabe3e0879c404ae720c399a83
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-17 22:43:19 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
- <head><title>TUM The Whole Welcome</title>
+ <head><title>TUM The Whole Welcome Memo</title>
 <meta charset="UTF-8" />
    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
