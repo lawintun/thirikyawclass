be1fb87f2afb73f44201fee9a4bda0b258c976b0
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 14:16:50 +0630

    Update index.php

--- index.php
+++ index.php
@@ -8,27 +8,15 @@
  </head>
    <body>
 
-<div class="login">
-  <h2>Login</h2>
-  <form>
-    <div class="mydiv">
+<center>
       <input type="text" id="usn" required="">
       <label>Username</label>
-    </div>
-    <div class="mydiv">
       <input type="password" id="pwd" required="">
       <label>Password</label>
-    </div>
-    <a href="#"><button class="invisible" onclick="myfunction()">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      Submit
+   <button onclick="myfunction()">
+     Submit
 </button>
-    </a>
-  </form>
-</div>
-
+   
+</center>
    </body>
 </html>
