f199acd9b07357f7133d665ccc60e48df9b96627
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-19 03:41:55 +0630

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 <script type="text/javascript" src="script.js">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
  
-<center><h3>TUM MEMO</h3></center>
+<center><div class="ht" ><h3>TUM MEMO</h3></div></center>
 
 <div class="container">
 <div class="header">
-<h1>Login</h1>
+<h1>Log In</h1>
 </div>
 
       
