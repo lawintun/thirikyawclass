0eaed630cd40ed12e61e3b68b243c90dc52ec386
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 21:33:58 +0630

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ header("location: home.php");
 else {
 alert("you are wrong !");
 }
-}  
+} 
+
+ (document).ready(function(){
+  $('#hideshow').on('click', function(event) {        
+     $('.content').toggle('show');
+  });
+});
+$(document).ready(function(){
+  $('#hideshow2').on('click', function(event) {        
+     $('.content2').toggle('show');
+  });
+});
 
 
