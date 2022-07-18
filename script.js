6318fa3e9a8235b7e13b1a92f0e960b1e62c8583
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 13:08:10 +0630

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ function func() {
 
 
 function myfunction(){
+if (document.getElementById("name").value == "dawpyae"){
 window.location.replace("home.php");
+}
+else {
+alert("you are wrong !");
+}
 }  
 
 
