1a2c7eb42b11d8200af497c59140e7967929c899
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 13:54:27 +0630

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,29 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <script type="text/javascript" src="script.js"></script>
   <link rel="stylesheet" href="style.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</head>
+ </head>
    <body>
 
-Name : <input type="text" id="name"/>
-<button onclick="myfunction()">Button</button>
+<div class="login">
+  <h2>Login</h2>
+  <form>
+    <div class="mydiv">
+      <input type="text" name="" required="">
+      <label>Username</label>
+    </div>
+    <div class="mydiv">
+      <input type="password" name="" required="">
+      <label>Password</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
+</div>
 
    </body>
 </html>
