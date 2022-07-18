2d6ff17e84e91d7bf4ba671cc6e599a2a3615999
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 21:53:16 +0630

    Update home.php

--- home.php
+++ home.php
@@ -12,13 +12,11 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <body>
 
-  <iframe class="responsive-iframe" id="myframe" src="https://drive.google.com/file/d/1QR5aE3rO0gmpUVy0J1WQFBRpW9wqGIwZ/preview" allow="autoplay"></iframe>
+  <iframe id="myframe" src="https://drive.google.com/file/d/1QR5aE3rO0gmpUVy0J1WQFBRpW9wqGIwZ/preview" allow="autoplay" style="width:100%;"></iframe>
 
-<iframe id="myiframe" src="https://en.wikipedia.org" style="height:450px;width:100%;"></iframe>
-
-<br /><br />
+<br /><br /><center>
 <button onclick="toggle(this);">Hide Iframe</button>
-
+</center>
 <script>
   let toggle = button => {
     let element = document.getElementById("myiframe");
