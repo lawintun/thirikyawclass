31939989facb568318620df5a9a1cacc57bc5f51
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 21:36:41 +0630

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,4 @@ alert("you are wrong !");
 }
 } 
 
- (document).ready(function(){
-  $('#hideshow').on('click', function(event) {        
-     $('.content').toggle('show');
-  });
-});
-$(document).ready(function(){
-  $('#hideshow2').on('click', function(event) {        
-     $('.content2').toggle('show');
-  });
-});
-
-
+ 
