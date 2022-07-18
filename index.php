968a91201715b25c3a9bdbf7bdd6731bc3dd90ba
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 17:27:04 +0630

    Update index.php

--- index.php
+++ index.php
@@ -12,9 +12,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
  
 <center>
+<h1>TUM THE WHOLE WELCOME MEMO</h2>
 <div class="container">
 <div class="header">
-<h1>TUM THE WHOLE WELCOME MEMO</h1>
+<h1>Login</h1>
 </div>
 <div class="main">
 <form>
