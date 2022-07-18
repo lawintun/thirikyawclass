60656eb5d71d7c295afe3d3cd2e3a8fb3acaf465
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 21:48:20 +0630

    Update home.php

--- home.php
+++ home.php
@@ -11,9 +11,9 @@
 <script type="text/javascript" src="script.js">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <body>
-<div class="container"> 
-  <iframe class="responsive-iframe" id="myframe" src="https://drive.google.com/file/d/1QR5aE3rO0gmpUVy0J1WQFBRpW9wqGIwZ/preview" allow="autoplay" style="height:450px;width:100%;"></iframe>
-</div>
+
+  <iframe class="responsive-iframe" id="myframe" src="https://drive.google.com/file/d/1QR5aE3rO0gmpUVy0J1WQFBRpW9wqGIwZ/preview" allow="autoplay"></iframe>
+
 <iframe id="myiframe" src="https://en.wikipedia.org" style="height:450px;width:100%;"></iframe>
 
 <br /><br />
