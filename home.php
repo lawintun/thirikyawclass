af2033d5a399e7b524e3e80f63395177a836587d
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 21:23:57 +0630

    Update home.php

--- home.php
+++ home.php
@@ -11,6 +11,23 @@
 <script type="text/javascript" src="script.js">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <body class="bg">
-<h>Home</h>
+<div class="navbar transparent navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                    <div class="container">
+                        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="brand" href="#">lol</a>
+                        <div class="nav-collapse collapse">
+                            <ul class="nav pull-right">
+                                <li class="active"><a href="/">Home</a></li>
+                                <li><a href="about">About</a></li>
+                            </ul>
+                        </div>
+                    </div>
+            </div>
+        </div>
 </body>
 </html>
