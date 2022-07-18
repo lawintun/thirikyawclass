a6e809c6a35c1c1cded674985c4f2b65e1f6affe
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 21:32:55 +0630

    Update home.php

--- home.php
+++ home.php
@@ -11,6 +11,20 @@
 <script type="text/javascript" src="script.js">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <body class="bg">
-
+<button id='hideshow'>Link1</button>
+<div class="panel">
+  <iframe src="linkcodehere" class="content" style="border:0;display:none"></iframe>
+  <br/>
+  <p class="content" style="display:none;">some stuff</p>
+  <p class="content" style="display:none;">more stuff</p>
+</div>
+<button id="hideshow2">Link2</button>
+<div class="panel2">
+  <iframe src="linkcodehere2" class="content2" style="border:0; display:none;"></iframe>
+  <br/>
+  <p class="content2" style="display:none;">some stuff</p>
+  <p class="content2" style="display:none;">more stuff</p>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </body>
 </html>
