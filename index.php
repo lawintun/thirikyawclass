307a6ae3b5fc719c5bc10193c80483f5f2301798
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 17:00:38 +0630

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,11 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
  
 <center>
+<div class="container">
+<div class="header">
+<h1>TUM THE WHOLE WELCOME MEMO</h1>
+</div>
+<div class="main">
 <form>
    <span>
       <input type="text" id="usn" placeholder="Username" required=""><br>
@@ -23,6 +28,8 @@
      Submit
 </button>
 </form>
+</div>
+</div>
 </center>
    </body>
 </html>
