f1f6d9d3cf5e80698ff8bf93aed7b39e77f76a5d
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 16:53:06 +0630

    Update index.php

--- index.php
+++ index.php
@@ -12,10 +12,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
  
 <center>
-      <input type="text" id="usn" required="">
-      <label>Username</label>
-      <input type="password" id="pwd" required="">
-      <label>Password</label>
+      <input type="text" id="usn" placeholder="Username" required=""><br>
+      <input type="password" id="pwd" placeholder="Password" required="">
    <button onclick="myfunction()">
      Submit
 </button>
