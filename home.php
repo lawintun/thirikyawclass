f71993770b93da398338b0e40f71dce455b71bf0
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 21:02:29 +0630

    Update home.php

--- home.php
+++ home.php
@@ -1,7 +1,16 @@
-
 <html>
-<head><title></title></head>
-<body>
+ <head><title>TUM The Whole Welcome Memo</title>
+<meta charset="UTF-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</script>
+  <link rel="stylesheet" href="style.css">
+ </head>
+   <body>
+<script type="text/javascript" src="script.js">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<body class="bg">
 <h>Home</h>
 </body>
 </html>
