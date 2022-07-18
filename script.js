862c6e3374eb92e954e484dce20f9af5448983c0
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-18 14:18:02 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function myfunction(){
 if ((document.getElementById("usn").value == "dawpyae")&&(document.getElementById("pwd").value == 192168128)) {
 window.location.replace("home.php");
+header("location: home.php");
 }
 else {
 alert("you are wrong !");
