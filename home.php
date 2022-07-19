4f3b91fbf4c6a3d8f259b63588df2e660b91e9f8
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-19 19:48:03 +0630

    Update home.php

--- home.php
+++ home.php
@@ -4,6 +4,83 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <style>
+*{
+margin:0; padding:0;
+border-sizing: border-box;
+text-transform: capitalize;
+font-family:Verdana,Geneva,Tahoma,san-serif;
+font-weight:normal;
+}
+body{
+background:#eee;
+}
+.heading{
+color:#444;
+font-size:40px;
+text-align:center;
+padding:10px;
+}
+.container {
+display:grid;
+grid-template-columns: 2fr 1fr;
+gap: 15px;
+align-items : flex-start;
+padding : 5px 5%;
+
+}
+.container .main-video{
+background : #fff;
+border-radius:5px;
+padding:10px;
+
+}
+
+.containter .main-video iframe{
+width:100%;
+border-radius:5px;
+}
+
+.containter .main-video .title{
+color:#333;
+font-size:23px;
+padding-top:25px;
+padding-bottom:25px;
+}
+.containter .video-list {
+background : #fff;
+border-radius : 5px;
+height:520px;
+overflow-y:scroll;
+}
+.containter .video-list::webkit-scrollbar{
+width:7px;
+}
+.containter .video-list::webkit-scrollbar-track{
+background: #ccc;
+border-radius:50px;
+}
+.containter .video-list::webkit-scrollbar-thumb{
+background:#666;
+border-radius:50px;
+}
+
+.containter .video-list .vid iframe{
+width:100px;
+border-radius:5px;
+}
+.containter .video-list .vid{
+display:flex;
+align-items:center;
+gap:15px;
+background:#f7f7f7;
+border-radius:5px;
+margin:10px;
+padding:10px;
+border: 1px solid rgba(0,0,0,0.1);
+cursor:pointer;
+}
+
+
 </style>
 </head>
    <body>
