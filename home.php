269a3ac6b741be576a67f2e506bca1062a15fa93
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-19 19:44:41 +0630

    Update home.php

--- home.php
+++ home.php
@@ -3,13 +3,10 @@
 <meta charset="UTF-8" />
    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-</script>
- <style>
+<style>
 </style>
 </head>
    <body>
-<script type="text/javascript" src="script.js">
-
 <h3 class="heading">TUM MEMO</h3>
 <div class="container">
 <div class="main-video">
