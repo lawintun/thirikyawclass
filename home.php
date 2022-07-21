3139bd23f21c5cb73ec8a991520ba87c0eab6cc3
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-22 03:10:48 +0630

    Update home.php

--- home.php
+++ home.php
@@ -189,13 +189,13 @@ storeID(i);
 <div class="container">
 <div class="main-video">
 <div class="video" id="main">
-<iframe class="responsive-iframe" id="MyFrame" src="https://onedrive.live.com/embed?cid=9A4ADCF2491E75C7&resid=9A4ADCF2491E75C7%21136&authkey=AMLWgDgaceGyAuU" allow="autoplay" frameborder="0" allow="accelerometer; autoplay; clipboard-write;" allowfullscreen></iframe>
+<iframe class="responsive-iframe" id="MyFrame" src="https://www.youtube-nocookie.com/embed/KLxUSuT4uc0" allow="autoplay" frameborder="0" allow="accelerometer; autoplay; clipboard-write;" allowfullscreen></iframe>
 <h3 class="title">TUM THE WHOLE FRESHER WELCOME -PREPARATION </h3>
 </div>
 </div>
 <div class="video-list">
 <div class="vid active" onclick="encoding(this.id)" id="act">
-<iframe class="responsive-iframe" id="MyFrame" src="https://onedrive.live.com/embed?cid=9A4ADCF2491E75C7&resid=9A4ADCF2491E75C7%21136&authkey=AMLWgDgaceGyAuU" allow="autoplay" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe class="responsive-iframe" id="MyFrame" src="https://www.youtube-nocookie.com/embed/KLxUSuT4uc0" allow="autoplay" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <h3 class="title">TUM THE WHOLE FRESHER WELCOME -PREPARATION </h3>
 </div>
 <div class="vid" onclick="encoding(this.id)" id="a">
