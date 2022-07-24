92d0c73215443a03d8b26c4d8c283777974e0209
lawintun <53369035+lawintun@users.noreply.github.com>
2022-07-24 19:39:11 +0630

    Update home.php

--- home.php
+++ home.php
@@ -256,8 +256,8 @@ storeID(i);
 <h3 class="title">TUM THE WHOLE FRESHER WELCOME PART- 2 </h3>
 </div>
 <div class="vid" onclick="encoding(this.id)" id="j">
-<iframe  class="responsive-iframe" id="MyFrame" src="https://www.youtube.com/embed/fla8ztStJdM" allow="autoplay" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<h3 class="title">TUM THE WHOLE FRESHER WELCOME PART-</h3>
+<iframe  class="responsive-iframe" id="MyFrame" src="https://www.youtube.com/embed/Vxj6SynDWDk" allow="autoplay" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<h3 class="title">TUM THE WHOLE FRESHER WELCOME PART- 1</h3>
 </div>
 <div class="vid" onclick="encoding(this.id)" id="jk">
 <iframe  class="responsive-iframe" id="MyFrame" src="https://www.youtube.com/embed/fla8ztStJdM" allow="autoplay" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
