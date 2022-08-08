09b90d22a478091d3262a016caa52268129b33d1
lawintun <53369035+lawintun@users.noreply.github.com>
2022-08-07 20:56:16 -0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,43 @@
+<html>
+ <head><title>TUM The Whole Welcome Memo</title>
+<meta charset="UTF-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</script>
+  <link rel="stylesheet" href="style.css">
+ </head>
+   <body>
+<script type="text/javascript" src="script.js">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+ 
+<center><div class="ht" ><h3>TUM MEMO</h3></div></center>
+
+<div class="container">
+<div class="header">
+<h1>Log In</h1>
+</div>
+
+      
+
+   
+      <input type="text" class="input" id="usn" placeholder="Username" required="">
+ 
+<br><br>
+  
+   <input type="password" class="input" id="pwd" placeholder="Password" required="">
+<br>  <br><br><center>
+<button class="col" onclick="myfunction()">
+     Submit
+</button></center>
+</div>
+<br>
+<br>
+<br>
+<br>
+<center>
+<marquee><h5>THIS APP IS PROVIDED BY "Media Service Volunteers Squad" OF 3IT ENGINEERING STUDENTS. </h5></marquee>
+</center>
+
+   </body>
+</html>
